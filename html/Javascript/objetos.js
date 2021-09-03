55e26393377effc9085a0da35fb37a10e354c70b
emilioc11 <emiliocemiliog@gmail.com>
2021-09-03 15:07:51 -0300

    objetos de JS

--- html/Javascript/objetos.js
+++ html/Javascript/objetos.js
@@ -1,4 +1,4 @@
-//Objeto
+//Objeto: es una variable que tiene propiedades.
 const cancion ={
 duracion :{
     minutos: 3,
@@ -10,4 +10,9 @@ título: "Sobre lo que es un objeto",
 };
 
 console.log (cancion);
-console.log (cancion .duracion);
+console.log (cancion.duracion);
+//llamas a la subpropiedad a partir de un punto y la referencia a esa subpropiedad.
+console.log (cancion.duracion.minutos);
+console.log(
+    `Hola ${cancion.autor.nombre}, ${cancion.autor.apellido}, tu cancion ${cancion.titulo} esta buena`
+);
